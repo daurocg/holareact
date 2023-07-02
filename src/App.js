39d2a72ef319c81f05c28377d38f8d1334dd4591
Dauro Grillo <dauro.grillo@inetum.com>
2023-07-02 08:59:05 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function SelectionScreen() {
+  const [modules, setModules] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://quizzfuntionscertifications.azurewebsites.net/api/getmodulosnumpregunta');
+      setModules(result.data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hola Mundo (  version react XD  )</h1>
-    </div>
+    <select>
+      <option value="">--Por favor selecciona--</option>
+      {modules.map((module, index) => <option key={index} value={module.modulo}>{module.modulo}</option>)}
+    </select>
   );
 }
 
-export default App;
+export default SelectionScreen;
