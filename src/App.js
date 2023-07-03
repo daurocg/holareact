ca92033c62f2ad1d286bc2763717733e8114ce76
Dauro Grillo <dauro.grillo@inetum.com>
2023-07-03 09:15:53 +0200

    5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 // App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import SelectionScreen from './SelectionScreen';
 import ConfirmationScreen from './ConfirmationScreen';
