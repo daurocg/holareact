296e5030de467a5c6f146a2ecf8940e9c63aa87d
Dauro Grillo <dauro.grillo@inetum.com>
2024-07-01 00:31:18 +0200

    update

--- src/SelectionScreen.js
+++ src/SelectionScreen.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
-import { useTranslation } from 'react-i18next'; // Importa useTranslation
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useTranslation } from 'react-i18next';
 
-
-function SelectionScreen({ onModuleSelect,language  }) {
+function SelectionScreen({ onModuleSelect, language }) {
   const [modules, setModules] = useState([]);
-  const { t } = useTranslation(); // Obtén la función de traducción
+  const { t } = useTranslation();
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(`https://quizzfuntionscertifications.azurewebsites.net/api/getmodulosnumpregunta?languaje=${language}`);
-      setModules(result.data);
+      // Ordena los módulos por nombre antes de establecerlos en el estado
+      const sortedModules = result.data.sort((a, b) => {
+        if (a.modulo < b.modulo) return -1;
+        if (a.modulo > b.modulo) return 1;
+        return 0;
+      });
+      setModules(sortedModules);
     }
 
     fetchData();
@@ -21,19 +26,24 @@ function SelectionScreen({ onModuleSelect,language  }) {
     const selectedModule = modules.find(module => module.modulo === event.target.value);
     onModuleSelect(selectedModule);
   };
-if (modules.length === 0) {
-  return <p>{t('loading')}</p>; // Usa la función de traducción
-}
 
-return (
-  <div className="form-group">
+  if (modules.length === 0) {
+    return <p>{t('loading')}</p>;
+  }
+
+  return (
+    <div className="form-group">
       <label>{t('pleaseSelectModule')}</label>
-    <select className="form-control" onChange={handleChange}>
-    <option value="">{t('pleaseSelect')}</option>
-      {modules.map((module, index) => <option key={index} value={module.modulo} >{module.modulo} ( {module.preguntas} {t('questions')} )</option>)}
-    </select>
-  </div>
-);
+      <select className="form-control" onChange={handleChange}>
+        <option value="">{t('pleaseSelect')}</option>
+        {modules.map((module, index) => (
+          <option key={index} value={module.modulo}>
+            {module.modulo} ({module.preguntas} {t('questions')})
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default SelectionScreen;
